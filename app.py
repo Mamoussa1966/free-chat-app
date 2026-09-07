86012534ba0a16d3182f08a311a5c91875f2afc4
Mohamed Ahmed Moussa <electroplating.engineer@gmail.com>
2026-09-07 17:39:19 +0300

    Update app.py
    import streamlit as st
    
    from main import run_app
    
    st.set_page_config(
        page_title="AI Council",
        page_icon="🏛️",
        layout="wide",
        initial_sidebar_state="expanded",
    )
    
    run_app()

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ import streamlit as st
 
 from main import run_app
 
-
 st.set_page_config(
     page_title="AI Council",
     page_icon="🏛️",
     layout="wide",
     initial_sidebar_state="expanded",
 )
-
 
 run_app()
